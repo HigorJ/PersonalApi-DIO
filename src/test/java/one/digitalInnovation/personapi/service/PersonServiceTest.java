9d759d63061a5fbe696e5ffc0a8cddee5fc24859
HigorJ <higor_sgomes@outlook.com>
2021-08-27 13:31:13 -0300

    Projeto PersonalAPI - Digital Innovation One

---
+++ src/test/java/one/digitalInnovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,50 @@
+package one.digitalInnovation.personapi.service;
+
+import one.digitalInnovation.personapi.dto.request.PersonDTO;
+import one.digitalInnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalInnovation.personapi.entity.Person;
+import one.digitalInnovation.personapi.repository.PersonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static one.digitalInnovation.personapi.utils.PersonUtils.createFakeDTO;
+import static one.digitalInnovation.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
